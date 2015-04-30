cded192360644ce837fee8ca6e70ce7802864f7e
Feefity <feefity@gmail.com>
2015-04-29 20:06:33 -0400

    Created the MedicalTourism WebApp project folder

---
+++ MedicalTourism-WebApp/MedicalTourism/MedicalTourism/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicalTourism.Startup))]
+namespace MedicalTourism
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MedicalTourism-WebApp/MedicalTourism/MedicalTourism/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MedicalTourism.Global" Language="C#" %>
