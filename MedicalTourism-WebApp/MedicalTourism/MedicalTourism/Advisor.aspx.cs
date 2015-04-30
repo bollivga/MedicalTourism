d4e4cbf566d1e3e2a0a1cd786fd883b2255642cd
Feefity <feefity@gmail.com>
2015-04-30 04:17:43 -0400

    Added a buncha various project files, deleted some too

---
+++ MedicalTourism-WebApp/MedicalTourism/MedicalTourism/Advisor.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MedicalTourism
+{
+    public partial class Advisor : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                //Populating a DataTable from database.
+                DataTable dt = this.GetData();
+
+                //Building an HTML string.
+                StringBuilder html = new StringBuilder();
+
+                //Table start.
+                html.Append("<table border = '1'>");
+
+                //Building the Header row.
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<th>");
+                    html.Append(column.ColumnName);
+                    html.Append("</th>");
+                }
+                html.Append("</tr>");
+
+                //Building the Data rows.
+                foreach (DataRow row in dt.Rows)
+                {
+                    html.Append("<tr>");
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        html.Append("<td>");
+                        html.Append(row[column.ColumnName]);
+                        html.Append("</td>");
+                    }
+                    html.Append("</tr>");
+                }
+
+                //Table end.
+                html.Append("</table>");
+
+                //Append the HTML string to Placeholder.
+                PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+            }
+        }
+    
+        private DataTable GetData() 
+        {   
+            string category = Request.Form["category"];
+            string country = Request.Form["country"];
+            string rating = Request.Form["rating"];
+
+            //string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection("user id=loganga;password=;server=titan.csse.rose-hulman.edu;Trusted_Connection=yes;database=MedicalTourism;connection timeout=30"))
+            {
+                using (SqlCommand cmd = new SqlCommand("DisplayCityResults", con) { CommandType = CommandType.StoredProcedure })
+                {
+                    cmd.Parameters.AddWithValue("@Country", country);
+                    cmd.Parameters.AddWithValue("@Category", category);
+                    cmd.Parameters.AddWithValue("@Rating", rating);
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    
+
+}
---
+++ MedicalTourism-WebApp/MedicalTourism/MedicalTourism/Planner.aspx
@@ -0,0 +1,56 @@
+﻿
+
+<html>
+<head>
+    <title>Medical Tourism Advisor</title>
+</head>
+<body>
+
+    <h1>Medical Tourism Advisor</h1>
+    <p><b>Please fill out this form regarding your desired medical procedure</b></p>
+    <p>Surgery Category:</p>
+    <select id="category">
+        <option value="Neurosrgy">Neurosurgery</option>
+        <option value="Cosmetic">Cosmetic</option>
+        <option value="Organ trns">Organ Transplant</option>
+        <option value="Orthopedic">Orthopedic</option>
+        <option value="Thoracic">Thoracic</option>
+        <option value="Endocrine">Endocrine</option>
+    </select>
+    <p>Country Preferred:</p>
+    <select id="country">
+        <option value="--">--</option>
+        <option value="Austria">Austria</option>
+        <option value="Brazil">Brazil</option>
+        <option value="Bulgaria">Bulgaria</option>
+        <option value="Chile">Chile</option>
+        <option value="China">China</option>
+        <option value="Egypt">Egypt</option>
+        <option value="England">England</option>
+        <option value="France">France</option>
+        <option value="Germany">Germany</option>
+        <option value="India">India</option>
+        <option value="Ireland">Ireland</option>
+        <option value="Italy">Italy</option>
+        <option value="Japan">Japan</option>
+        <option value="Norway">Norway</option>
+        <option value="S.Korea">South Korea</option>
+        <option value="Scotland">Scotland</option>
+        <option value="Spain">Spain</option>
+        <option value="Sweden">Sweden</option>
+        <option value="Ukraine">Ukraine</option>
+    </select>
+    <p>Minimum Rating (Out of 5):</p>
+    <select id="rating">
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+    </select>
+    <p></p>
+    <button type="submit" id="submit">Submit</button>
+
+</body>
+</html>
